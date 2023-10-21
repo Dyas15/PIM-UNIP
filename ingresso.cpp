357e9481a1002d5d3d61ccc90be00a5b4be6c883
Diogo <98923655+Dyas15@users.noreply.github.com>
2023-10-21 01:30:30 -0300

    Validação de CPF

--- ingresso.cpp
+++ ingresso.cpp
@@ -25,25 +25,103 @@ void limparTela()
     system("cls");
 }
 
-//Função para verificar o CPF
+// Função para verificar o CPF
 int verificarCPF(char *cpf)
 {
-    //Se o tamanho do CPF for diferente de 11, então ele retorna inválido
+    // Declaração de variaveis para guardar os numeros verificadores
+    // São os numeros finais xxx.xxx.xxx-VV
+    int verificador1 = 0, verificador2 = 0;
+    // Se o tamanho do CPF for diferente de 11, então ele retorna inválido
     if (strlen(cpf) != 11)
     {
         return 0;
     }
 
-    //Irá fazer um laço para cada digito
+    // Irá fazer um laço para cada digito
     for (int i = 0; i < 11; i++)
     {
-        //Função isdigit serve para verificar se todos os valores do CPF são numeros e não outros caracteres.
+        // Função isdigit serve para verificar se todos os valores do CPF são numeros e não outros caracteres.
         if (!isdigit(cpf[i]))
         {
             return 0;
         }
     }
-    //Caso a validação funcionar, ele retorna true
+
+    // Função strcmp serve para comparar uma string com outra string
+    // Nesse caso é necessario pois o calculo do CPF não cobre numeros repetidos e deixa passar
+    // Caso seja digitado algum CPF dessa lista, retornará falso
+    if (strcmp(cpf, "00000000000") == 0 || strcmp(cpf, "11111111111") == 0 || strcmp(cpf, "22222222222") == 0 ||
+        strcmp(cpf, "33333333333") == 0 || strcmp(cpf, "44444444444") == 0 || strcmp(cpf, "55555555555") == 0 ||
+        strcmp(cpf, "66666666666") == 0 || strcmp(cpf, "77777777777") == 0 || strcmp(cpf, "88888888888") == 0 ||
+        strcmp(cpf, "99999999999") == 0)
+    {
+        return 0;
+    }
+
+    // Calculo do 1 digito verificador
+    // O calculo se resume a você multiplicar de 10 ao 1 cada numero do cpf em sequencia
+    // Ex: CPF: 837.735.680 com os digitos verificadores 53
+    // 8*10 + 3*9 + 7*8 + 7*7 + 3*6 + 5*5 + 6*4 + 8*3 + 0*2 == 303
+    for (int numero = 0, multiplicador = 10; numero <= 8; numero++, multiplicador--)
+    {
+        verificador1 += (cpf[numero] - '0') * multiplicador;
+    }
+
+    // Após calcular a soma de todas as multiplicações, fazemos a divisão pelo total de numeros do cpf e recebemos o resto da divisão
+    verificador1 %= 11;
+    // Caso o resto da divisão for 0 ou 1, então o digito verificador será automaticamente 0
+    if (verificador1 == 0 || verificador1 == 1)
+    {
+        verificador1 = 0;
+    }
+    else
+    {
+        // Caso o resto da divisão seja maior que 1, então é feito a conta da quantidade de numeros em um cpf
+        // menos o resultado do resto da divisão
+        // Nesse exemplo, resultando em 5, que está correto
+        verificador1 = 11 - verificador1;
+    }
+
+    // Verificação necessaria para comparar se o primeiro digito verificador do cpf é igual ao calculado
+    // Caso não seja, retornará falso
+    if ((cpf[9] - '0') != verificador1)
+    {
+        return 0;
+    }
+
+    // Calculo do 2 digito verificador
+    // O calculo se resume a você multiplicar de 11 ao 1 cada numero do cpf em sequencia
+    // A diferença desse para o primeiro digito verificador é que agora usamos o primeiro digito verificador na conta
+    // 8*11 + 3*10 + 7*9 + 7*8 + 3*7 + 5*6 + 6*5 + 8*4 + 0*3 + 5*2  == 360
+    for (int numero = 0, multiplicador = 11; numero <= 9; numero++, multiplicador--)
+    {
+        verificador2 += (cpf[numero] - '0') * multiplicador;
+    }
+
+    // Após calcular a soma de todas as multiplicações, fazemos a divisão pelo total de numeros do cpf e recebemos o resto da divisão
+    verificador2 %= 11;
+
+    // Caso o resto da divisão for 0 ou 1, então o digito verificador será automaticamente 0
+    if (verificador2 == 0 || verificador2 == 1)
+    {
+        verificador2 = 0;
+    }
+    else
+    {
+        // Caso o resto da divisão seja maior que 1, então é feito a conta da quantidade de numeros em um cpf
+        // menos o resultado do resto da divisão
+        // Nesse exemplo, resultando em 5, que está correto
+
+        verificador2 = 11 - verificador2;
+    }
+
+    // Verificação necessaria para comparar se o primeiro digito verificador do cpf é igual ao calculado
+    // Caso não seja, retornará falso
+    if ((cpf[10] - '0') != verificador2)
+    {
+        return 0;
+    }
+    // Caso a validação não for interrompida por nenhum erro, ele retorna true
     return 1;
 }
 
@@ -76,7 +154,7 @@ int main()
     while (1 != 0)
     {
 
-        //LGDP
+        // LGDP
         wprintf(L"Prezado(a) usuário(a),\nRespeitamos a sua privacidade e estamos comprometidos em proteger os seus dados pessoais.\n\n");
         wprintf(L"De acordo com a Lei Geral de Proteção de Dados (LGPD), precisamos da sua autorização para coletar, armazenar e utilizar as suas informações pessoais.\n\n");
         wprintf(L"Essas informações serão usadas exclusivamente para melhorar a sua experiência em nossa plataforma e garantir que nossos serviços atendam às suas necessidades da melhor maneira possível.\n\n");
@@ -84,7 +162,7 @@ int main()
         wprintf(L"Você pode retirar seu consentimento a qualquer momento.\n\n");
         wprintf(L"Você aceita?\n1)Sim\n2)Não\nR:");
         scanf("%d", &escolha);
-        //Se a pessoa aceitar os termos, seguira com a compra dos ingressos
+        // Se a pessoa aceitar os termos, irá seguir com a compra dos ingressos
         if (escolha == 1)
         {
             limparTela();
@@ -93,34 +171,35 @@ int main()
             printf("Digite seu nome: ");
             scanf("%s", &nome);
 
-            //Irá ficar repetindo até a pessoa colocar um CPF valido
+            // Irá ficar repetindo até a pessoa colocar um CPF valido
             do
             {
                 wprintf(L"Digite seu CPF sem traços ou pontuações: ");
                 scanf("%s", &*cpf);
-                //Chama a função verificarCPF passando o cpf escrito pela pessoa.
-                //Uma função IF sempre retornara apensa valores booleanos
-                //Então, quando colocamos a função verificarCPF sem verificação de valores (EX: verificarCPF(cpf) == true)
-                //automaticamente irá verificar se é verdadeiro ou falso o resultado da função
+                // Chama a função verificarCPF passando o cpf escrito pela pessoa.
+                // Uma função IF sempre retornara apensa valores booleanos
+                // Então, quando colocamos a função verificarCPF sem verificação de valores (EX: verificarCPF(cpf) == true)
+                // automaticamente irá verificar se é verdadeiro ou falso o resultado da função
+
                 if (verificarCPF(cpf))
                 {
-                    //Sai do loop
+                    // Sai do loop
                     break;
                 }
                 else
                 {
-                    //Auto-explicativo
+                    // Auto-explicativo
                     limparTela();
                     wprintf(L"CPF Inválido!");
-                    //Espera por 3 segundos
+                    // Espera por 3 segundos
                     Sleep(3000);
-                    //Auto-explicativo
+                    // Auto-explicativo
                     limparTela();
                 }
-                //O loop é infinito até a pessoa colocar o CPF corretante
+                // O loop é infinito até a pessoa colocar o CPF corretamente
             } while (1 != 0);
 
-            //Auto-explicativo
+            // Auto-explicativo
             limparTela();
 
             printf("Escolha seu ingresso:\n");
